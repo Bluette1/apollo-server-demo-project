5fffa88f203ea623d3d09b41ddd7f5f9e7e06323
Bluette1 <marylene.sawyer@gmail.com>
2021-04-04 16:04:24 +0200

    Add mutation query

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,28 @@
-import { IResolvers } from "graphql-tools";
+import { IResolvers } from 'graphql-tools';
+let links: Object[] = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
+let idCount: number = links.length;
 
 const resolvers: IResolvers = {
   Query: {
-    helloWorld: () => "Hello world from Apollo Server",
+    helloWorld: () => 'Hello world from Apollo Server',
+  },
+  Mutation: {
+    post: (_parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      console.log(links);
+      return link;
+    },
   },
 };
 
